b2d49165fda074e7b835eeb3f2b5960a0c2c7ce4
DeveloperMan313 <053000voloshch@gmail.com>
2025-11-24 02:27:31 +0300

    Configure tauri

--- src/main.tsx
+++ src/main.tsx
@@ -4,20 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import store from "./services/store";
 import { Provider } from "react-redux";
-import { registerSW } from "virtual:pwa-register";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/main.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/dia-frontend">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 );
-
-if ("serviceWorker" in navigator) {
-  registerSW();
-}
