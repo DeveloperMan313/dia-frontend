d9fdbd6ead83365a11ed19d7665689daa51a7c78
DeveloperMan313 <053000voloshch@gmail.com>
2025-12-10 12:28:02 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/dia-frontend/sw.js', { scope: '/dia-frontend/' })})}
